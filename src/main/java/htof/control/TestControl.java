14011f773f3804a8aa641412ccbed1179e3c8284
miaoch <476864279@qq.com>
2017-10-30 16:59:45 +0800

    first

---
+++ src/main/java/htof/control/TestControl.java
@@ -0,0 +1,39 @@
+package htof.control;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by miaoch on 2017/10/30.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestControl {
+    private static Logger logger = LoggerFactory.getLogger("TestControl");
+
+    @RequestMapping
+    @ResponseBody
+    public String test(HttpServletRequest request) {
+        logger.debug(request.getParameter("requestId"));
+        logger.debug(request.getParameter("type"));
+        logger.debug("debug--------");
+        logger.info("info--------");
+        logger.error("error--------");
+        JSONObject json = new JSONObject();
+        try {
+            json.put("message", "ok");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json.toString();
+    }
+}
